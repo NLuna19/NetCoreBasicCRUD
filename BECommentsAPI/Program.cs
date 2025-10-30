81f4058b5cc2ddfed24bf8bec374ac2676ff47b1
nluna19 <nluna190898@gmail.com>
2025-10-29 21:58:39 -0300

    url lowercase

--- BECommentsAPI/Program.cs
+++ BECommentsAPI/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder => 
